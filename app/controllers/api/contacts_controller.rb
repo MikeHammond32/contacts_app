0987a37db6f928833abbb0355164e01a730d249b
Mike <mikehammond32@gmail.com>
2019-07-01 21:21:57 -0400

    creates a update action

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -21,13 +21,15 @@ class Api::ContactsController < ApplicationController
    @contact.save
    render 'create.json.jb'
   end
+  def update
+    the_id = params[:id]
+    @contact = Contact.find_by(id: params[:id])
+    @contact.first_name = params[:first_name]
+    @contact.last_name = params[:last_name]
+    @contact.email = params[:email]
+    @phone_number = params[:phone_number]
+    @contact.save
+    render 'update.json.jb'
+  end
 end
 
-
-   # the_id = params[:id]
-    # @contact = Contact.find_by(id: params[:id])
-    # @contact.first_name = params[:first_name]
-    # @contact.last_name = params[:last_name]
-    # @contact.email = params[:email]
-    # @phone_number = params[:phone_number]
-  
