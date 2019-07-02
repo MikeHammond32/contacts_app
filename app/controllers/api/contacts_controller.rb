601de7f5191d2e1d0450e29486a3cc51d9f97fe8
Mike <mikehammond32@gmail.com>
2019-07-02 19:28:46 -0400

    added an user friendly updated time, as well as a full name function

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -24,10 +24,10 @@ class Api::ContactsController < ApplicationController
   def update
     the_id = params[:id]
     @contact = Contact.find_by(id: params[:id])
-    @contact.first_name = params[:first_name]
-    @contact.last_name = params[:last_name]
-    @contact.email = params[:email]
-    @phone_number = params[:phone_number]
+    @contact.first_name = params[:first_name] || @contact.first_name
+    @contact.last_name = params[:last_name] || @contact.last_name
+    @contact.email = params[:email] || @contact.email
+    @phone_number = params[:phone_number] || @contact.phone_number
     @contact.save
     render 'update.json.jb'
   end
