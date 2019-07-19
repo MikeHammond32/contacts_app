07486e1b06781adc406960f4bdfd208e4ffb4885
Mike <mikehammond32@gmail.com>
2019-07-18 22:32:45 -0400

    created html.erb for edit, index, and new

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,12 @@ Rails.application.routes.draw do
     post "/users" => "users#create"
     post "/sessions" => "sessions#create"
   end
-    get '/recipes' =>
+    get '/recipes' => 'recipes#index'
+    get '/recipes/new' => 'recipes#new'
+    get '/recipes/:id' => 'recipes#show'
+    post '/recipes' => 'recipes#create'
+    get '/recipes/:id/edit' => 'recipes#edit'
+    patch '/recipes/:id' => 'recipes#update'
+    delete '/recipes/:id' => 'recipes#destroy'
 
 end
