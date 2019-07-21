e17668d1608397722b9c8aebb83b44053159285f
Mike <mikehammond32@gmail.com>
2019-07-20 22:28:01 -0400

    changed the apostrophies to be more consitent in the routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   # EXAMPLE HTML ROUTE
   namespace :api do
-    get "/contacts" => "contacts#index"
-    get "/contacts/:id" => "contacts#show"
-    post "/contacts" => "contacts#create"
-    patch "/contacts/:id" => "contacts#update"
-    post "/users" => "users#create"
-    post "/sessions" => "sessions#create"
+    get '/contacts' => 'contacts#index'
+    get '/contacts/:id' => 'contacts#show'
+    post '/contacts' => 'contacts#create'
+    patch '/contacts/:id' => 'contacts#update'
+    post '/users' => 'users#create'
+    post '/sessions' => 'sessions#create'
   end
     get '/recipes' => 'recipes#index'
     get '/recipes/new' => 'recipes#new'
